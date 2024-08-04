431c86a5f92d91ceb79303bf9bda828f0ee1b4dd
0xp17j8ff01 <111459558+0xp17j8@users.noreply.github.com>
2024-08-04 13:57:03 +0100

    SignatureSlip.h
    just a header for fuck DSE, soon ->

---
+++ SignatureSlip/SignatureSlip.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFFER_SIZE MAX_PATH * 2
+#define VBoxDrvDispName L"Steam Drivers"
+#define VBoxDrvRegPath   L"\\Registry\\Machine\\System\\CurrentControlSet\\Services\\vboxdrv"
+#define VBoxDrvDevName  L"\\Device\\VBoxDrv"
+
+#endif

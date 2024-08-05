6ddbed706c98ac278574af0e46ecab495da5308b
0xp17j8ff01 <111459558+0xp17j8@users.noreply.github.com>
2024-08-05 02:12:44 +0100

    SignatureSlip.h

--- SignatureSlip/SignatureSlip.h
+++ SignatureSlip/SignatureSlip.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "ntdll\ntdll.h"
+#include "ntdll\ntstatus.h"
+#include "SignatureSlip.h"
+#include "vbox.h"
+#include "vboxdrv.h"
+#include "ldasm.h"
+#include "rtls\prtl.h"
+#include "ntdll\winnative.h"
+
 #define BUFFER_SIZE MAX_PATH * 2
 #define VBoxDrvDispName L"Steam Drivers"
 #define VBoxDrvRegPath   L"\\Registry\\Machine\\System\\CurrentControlSet\\Services\\vboxdrv"
